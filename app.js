5c510da9a21fe64829853874d1c55f4fa3176f29
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-30 16:51:29 +0900

    Add: GET /challenges/:id

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
+const challengesRouter = require("./routes/challenges");
 const { handleNotFound, handleDefaultError } = require("./errorHandler");
 
 const app = express();
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use("/", indexRouter);
+app.use("/challenges", challengesRouter);
 
 app.use(handleNotFound);
 app.use(handleDefaultError);
